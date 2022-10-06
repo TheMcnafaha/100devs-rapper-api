904255dc3454482b96de9a323dab3c428e8dd165
Jafet Guzman <natanswebservices@gmail.com>
2022-10-06 15:44:39 -0400

    added index.html ammend fixed html dir & .gitignore to git

--- server.js
+++ server.js
@@ -3,10 +3,14 @@ const app=express()
 
 const PORT=8000
 
+app.get(`/`,(req,res)=>{
+    res.sendFile(__dirname+`/html/index.html`)
+})
 
 
 
 
 
-
-app.listen(PORT)
+app.listen(PORT,_=>{
+console.log(`online`)
+})
